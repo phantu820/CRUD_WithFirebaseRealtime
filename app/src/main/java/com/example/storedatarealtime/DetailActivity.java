13aa0d07207b820dd69a37d456c44f8c28ae3fc0
tudeptrai <tu416164@gmail.com>
2023-10-01 12:03:57 +0700

    up crud firebaserealtime

---
+++ app/src/main/java/com/example/storedatarealtime/DetailActivity.java
@@ -0,0 +1,110 @@
+package com.example.storedatarealtime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.github.clans.fab.FloatingActionButton;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
+public class DetailActivity extends AppCompatActivity {
+
+    TextView detailDesc, detailTitle, detailLang;
+    ImageView detailImage;
+    FloatingActionButton deleteButton, editButton;
+    String key = "";
+    String imageUrl = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        detailDesc = findViewById(R.id.detailDesc);
+        detailImage = findViewById(R.id.detailImage);
+        detailTitle = findViewById(R.id.detailTitle);
+        deleteButton = findViewById(R.id.deleteButton);
+        editButton = findViewById(R.id.editButton);
+        detailLang = findViewById(R.id.detailLang);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null){
+            detailDesc.setText(bundle.getString("Description"));
+            detailTitle.setText(bundle.getString("Title"));
+            detailLang.setText(bundle.getString("Language"));
+            key = bundle.getString("Key");
+            imageUrl = bundle.getString("Image");
+            Glide.with(this).load(bundle.getString("Image")).into(detailImage);
+        }
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a confirmation dialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(DetailActivity.this);
+                builder.setTitle("Confirm Delete");
+                builder.setMessage("Are you sure you want to delete this item?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User clicked Yes, perform the delete operation
+                        deleteItem();
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User clicked No, dismiss the dialog
+                        dialog.dismiss();
+                    }
+                });
+
+                // Show the confirmation dialog
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DetailActivity.this, UpdateActivity.class)
+                        .putExtra("Title", detailTitle.getText().toString())
+                        .putExtra("Description", detailDesc.getText().toString())
+                        .putExtra("Language", detailLang.getText().toString())
+                        .putExtra("Image", imageUrl)
+                        .putExtra("Key", key);
+                startActivity(intent);
+            }
+        });
+    }
+    private void deleteItem() {
+        final DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Android Tutorials");
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+
+        StorageReference storageReference = storage.getReferenceFromUrl(imageUrl);
+        storageReference.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                reference.child(key).removeValue();
+                Toast.makeText(DetailActivity.this, "Deleted", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        });
+    }
+
+}
